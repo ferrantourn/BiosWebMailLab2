c4655d7075395c02c194efca04ca7ab328213ec8
Amalfi Marini <fito@fito.com>
2012-12-01 18:46:00 -0300

    logica usuario corregida
    logica usuario corregida

--- Logica/LogicaUsuario.cs
+++ Logica/LogicaUsuario.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Xml;
 using Entidades;
 using Persistencia;
+using System.Xml;
 
 namespace Logica
 {
@@ -173,19 +174,7 @@ namespace Logica
             }
 
         }
-        public XmlDocument ListarEstadistica()
-        {
-            try
-            {
-                IPersistenciaAlumnos persAlumnos = FabricaPersistencia.getPersistenciaAlumnos();
-                return persAlumnos.ListarEstadistica();
-            }
-            catch (Exception ex)
-            {
-                throw ex;
-            }
-
-        }
+ 
 
         public List<Docente> ListarDocentes()
         {
